3ff9e32fc00ca0ad83ac8022672ff168dd03b2d5
jannatulferdos05 <jannatulferdos587@gmail.com>
2024-06-29 01:27:40 +0600

    tailwind2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,10 @@
 <body class="h-screen flex items-center justify-center mx-auto bg-gray-100">
   <div class="max-w-sm bg-white mx-auto p-8 rounded-lg shadow-md ">
     <img class="h-24 w-24 mx-auto rounded-full ring-4 ring-green-400 " src="/images/me.jpg"  >
-    <div class="">
-      <p class="text-lg font-semibold">Learn with Jui</p>
-      <p class="text-gray-500">YouTube Channel</p>
-      <button class="px-4 py-1 rounded-full p-1 bg-blue-600   hover:bg-green-800 ">View</button>
-
+    <div class="text-center">
+      <p class="text-lg text-black font-semibold">Learn with Jui</p>
+      <p class="text-gray-500 font-medium">YouTube Channel</p>
+      <button class="px-4 py-1 border rounded-full text-sm font-semibold  hover:bg-green-400 ">View</button>
     </div>
   </div>
 </body>
