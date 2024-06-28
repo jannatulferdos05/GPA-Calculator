40e6282443a80db91120042623f64281097431d1
jannatulferdos05 <jannatulferdos587@gmail.com>
2024-06-29 01:10:40 +0600

    card-design-tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
-<body>
-  <h1 class="text-3xl bg-red-400 ">
-    Hello world!
-  </h1>
+<body class="h-screen flex items-center justify-center mx-auto bg-gray-100">
+  <div class="max-w-sm bg-white mx-auto p-8 rounded-lg shadow-md ">
+    <img class="h-24 w-24 mx-auto rounded-full ring-4 ring-green-400 " src="/images/me.jpg"  >
+    <div class="">
+      <p class="text-lg font-semibold">Learn with Jui</p>
+      <p class="text-gray-500">YouTube Channel</p>
+      <button class="px-4 py-1 rounded-full p-1 bg-blue-600   hover:bg-green-800 ">View</button>
+
+    </div>
+  </div>
 </body>
 </html>
