f06bccca41420e158966c6ead1488e1c9af5e81c
jannatulferdos05 <jannatulferdos587@gmail.com>
2024-07-01 03:17:12 +0600

    flexbox-margin

--- resources/views/flexbox.blade.php
+++ resources/views/flexbox.blade.php
@@ -9,23 +9,44 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
         <style>
-            .container{
-                display:flex;
-               
-                justify-content: center;
-               
-                color: crimson ;
+            .navbar{
+                display: flex;
+                flex-wrap: wrap;
+                justify-content:space-between;
+                align-items: center;
+                flex-wrap: wrap;
+                margin-left:0px;
+                margin-right:0px;
+                background-color: teal;
             }
-
+            .navItem{
+                
+                border-radius:4px;
+                flex-basis:120px;
+               
+            }
+            .logo{
+                height:50px;
+                width:50px;
+                border-radius:25px;
+                margin-left:70px;
+            }
+            .search{
+                border-radius:1px;
+                width:120px;
+                margin-right:70px;
+            }
         </style>
     </head>
     <body>
-        <div class="container">   
-            <div>HOME</div>
-            <div>PROJECTS</div>
-            <div>RESEARCHS</div>
-            <div>ABOUT</div>
-            <button>Login</button>
+        <div class="navbar"> 
+            <img class="logo" src="/images/me.jpg">  
+            <div class="navItem">HOME</div>
+            <div class="navItem">PROJECTS</div>
+            <div class="navItem">RESEARCHS</div>
+            <div class="navItem">ABOUT</div>
+            <button class="navItem">Login</button>
+            <input class="search" placeholder="Type to Search">
         </div>
         <script src="" async defer></script>
     </body>
